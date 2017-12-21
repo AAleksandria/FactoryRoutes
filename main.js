09d3a42067259bfe9215485b9350b63179128917
AAleksandria <aleksandrsadchikov.17@gmail.com>
2017-12-21 13:23:35 +0200

    change index.html & main.js

--- main.js
+++ main.js
@@ -3,16 +3,6 @@ const {app, BrowserWindow, Menu} = electron
 const path = require('path')
 const url = require('url')
 
-// шаблон меню
-// menuTemplate = [
-//   {
-//     label: 'О программе',
-//     click: () => {
-//       openAboutWindow()
-//     }
-//   }
-// ]
-
 // отвечает за главное окно приложение
 let mainWindow
 
@@ -29,8 +19,6 @@ function createWindow () {
     slashes: true
   }))   // загружает index.html как шаблон для главной страницы приложения
 
-  // var menu = Menu.buildFromTemplate(menuTemplate)   // создаёт шаблон меню
-  // mainWindow.setMenu(menu)
 
   mainWindow.on('closed', () => {
     mainWindow = null
@@ -38,30 +26,6 @@ function createWindow () {
 
   mainWindow.setMinimumSize(580, 400);              // задаёт минимальный размер 
 }
-
-// // создаёт окно "О программе" 
-// function openAboutWindow() {
-
-//   let aboutWindow = new BrowserWindow({
-//     parent: mainWindow,
-//     modal: true,
-//     show: false,
-//     icon: path.join(__dirname, '/files/GoogleMaps.ico')
-//   })    // создание окно "О программе"
-
-//   aboutWindow.loadURL(url.format({
-//     pathname: path.join(__dirname, '/particles/about.html'),
-//     protocol: 'file:',
-//     slashes: true
-//   }))   // загружает about.html как шаблон
-
-//   aboutWindow.setMenu(null)                         // окно без меню
-
-//   aboutWindow.once('ready-to-show', () => {
-//     aboutWindow.maximize()
-//     aboutWindow.show()
-//   })    // вывод окна по готовности
-// }
 
 // работа с самим приложением, события приложения  
 app.on('ready', () => {
